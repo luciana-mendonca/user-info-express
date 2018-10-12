ae234886c648b51f251c878acef5a48e3784878b
Luci-Lawless <lu.gmmm@gmail.com>
2018-10-12 16:29:27 +0200

    Add layout with bootstrap.

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("about to render");
     fs.readFile('./users.json', 'utf-8', function (error, data){
-      console.log("readfile is working");
       if (error) {
         throw error;
       }
