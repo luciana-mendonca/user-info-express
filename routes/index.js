e32307788dea661bca4ecf1ca2a341e8a5388991
Luci-Lawless <lu.gmmm@gmail.com>
2018-10-15 20:03:14 +0200

    Search user by name and render results

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,45 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+// All Users
+router.get('/', function(request, response) {
     fs.readFile('./users.json', 'utf-8', function (error, data){
       if (error) {
         throw error;
       }
       var userList = JSON.parse(data);
-      res.render('index', {userList});
+      response.render('index', {userList});
     });
 });
 
-router.get('/search', function(req, res){
-  res.render('search');
+// Search bar
+router.get('/search', function(request, response) {
+  response.render('search');
+});
+
+router.post('/search', function(request, response) {
+  console.log(request.body.userInput);
+  fs.readFile('./users.json', 'utf-8', function (error, data){
+    if (error) {
+      throw error;
+    }
+    var userList = JSON.parse(data);
+    console.log(userList);
+    var searchResult = [];
+    for (var i = 0; i < userList.length; i++) {
+      if (userList[i].firstname.indexOf(request.body.userInput) > -1 || userList[i].lastname.indexOf(request.body.userInput) > -1){
+        searchResult.push(userList[i]);
+      }
+    }
+    response.render('results', {searchResult});
+  });
+});
+
+// Results
+
+router.get('/results', function(request, response) {
+  response.render('results');
 });
 
 module.exports = router;
