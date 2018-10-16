f82ed70f1ba5b133bdc7cc9f2ae429b58e6158c1
Luci-Lawless <lu.gmmm@gmail.com>
2018-10-16 19:45:51 +0200

    Add datalist to search box autocomplete

--- public/javascripts/autocomplete-search.js
+++ public/javascripts/autocomplete-search.js
@@ -2,10 +2,8 @@ $("#user-input").keyup(function() {
     var userInput = $(this).val();
     $("#sugestions").empty();
     $.post('/autocomplete', {userInput:userInput}, function (response, status) {
-        console.log(status)
-        console.log(response);
-        for (var i = 0; i < response.length; i++) {
-          $("#sugestions").append("<p>" + response[i].firstname + " " + response[i].lastname + " Email: " + response[i].email + "</p>")
-        }
+      for (var i = 0; i < response.length; i++) {
+        $("#sugestions").append("<option value='" + response[i].firstname + " " + response[i].lastname + "'></option>")
+      }
     });
 });
